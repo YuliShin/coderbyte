4529312b0f1536002a79866b0954f6164f702186
YuliShin <glynnisshin@naver.com>
2015-07-27 23:59:51 +0900

    Create SecondGreatLow.rb

---
+++ SecondGreatLow.rb
@@ -0,0 +1,9 @@
+def SecondGreatLow(arr)
+
+  # code goes here
+  return [arr.sort[+1], arr.sort[-2]].join(" ")
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SecondGreatLow(STDIN.gets)        
