810d1dd289f6573ebb946f6fe2ffaaf1adedf8aa
YuliShin <glynnisshin@naver.com>
2015-07-30 18:36:57 +0900

    Update CheckNums.rb

--- CheckNums.rb
+++ CheckNums.rb
@@ -1,12 +1,14 @@
 def CheckNums(num1,num2)
 
   # code goes here
-  flag = true
-  if num2 > num1
-    flag = true
-  else 
-    flag = -1
-  end
+  flag = 
+   if num2 > num1
+        "true"
+      elsif num2 < num1
+        "false"
+      else
+        "-1"
+      end
     
   return flag
          
